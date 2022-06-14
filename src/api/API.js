2f3ceb2bc30d663c9afc87e54263997bf6d0c3ea
anna.arlig <you@example.com>
2022-06-14 13:21:43 +0200

    Add https to functions

--- src/api/API.js
+++ src/api/API.js
@@ -3,7 +3,7 @@ import axios from "axios"
 
 export async function getDepartures(){
 try{
-return await axios.get( `http://localhost:8888/.netlify/functions/get-departures`)
+return await axios.get( `https://localhost:8888/.netlify/functions/get-departures`)
 }
 catch(error){
 console.log(error)
@@ -12,7 +12,7 @@ console.log(error)
 
 export async function getWeather(){
     try{
-    return await axios.get( `http://localhost:8888/.netlify/functions/get-weather`)
+    return await axios.get( `https://localhost:8888/.netlify/functions/get-weather`)
     }
     catch(error){
     console.log(error)
